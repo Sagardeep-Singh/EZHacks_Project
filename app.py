678efd6f79cd398660cf04ac309fe0f07bebc0bc
Sagardeep Singh Hunjan <sagardeep.0019@gmail.com>
2021-03-06 13:48:41 -0800

    Added templates/, static/, static/css, style.css Fixed templates locations. Removed a mistyped "a" from the do_something function Added code to handle get requests on the /join endpoint

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 from flask import Flask, request, render_template,jsonify
 app = Flask(__name__)
+
 def do_something(text1,text2):
    text1 = text1.upper()
    text2 = text2.upper()
-   combine = text1 +a text2
+   combine = text1 + text2
    return combine
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
 @app.route('/join', methods=['GET','POST'])
 def my_form_post():
-    text1 = request.form['text1']
-    word = request.args.get('text1')
-    text2 = request.form['text2']
+    if request.method == 'POST':
+        text1 = request.form['text1']
+        text2 = request.form['text2']
+    else:
+        text1 = request.args.get('text1')
+        text2 = request.args.get('text2')
+
     combine = do_something(text1,text2)
     result = {
         "output": combine
     }
-    result = {str(key): value for key, value in result.items()}
+    # result = {str(key): value for key, value in result.items()}
     return jsonify(result=result)
+
 if __name__ == '__main__':
     app.run(debug=True)
